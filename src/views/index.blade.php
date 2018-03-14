c2de6af06040862f2f0ab80649c912f168e7b9a6
dohuuson10@gmail.com <leo01226702638>
2018-03-14 09:28:49 +0700

    submit
    submit

---
+++ src/views/index.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Chuyên đề web 1</title>
+	<link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+	<link href="{{asset("foostart\css\app.css")}}" rel="stylesheet" type="text/css"/>
+</head>
+<body>
+		<div class="container">
+			<div class="content">
+				<div class="title""><?php echo $name ?></div>
+				<div class="name">Chuyên đề web 1 - Thầy Nhuần</div>
+				<img src="sample/wwww.jpg">
+			</div>
+		</div>
+</body>
+</html>
